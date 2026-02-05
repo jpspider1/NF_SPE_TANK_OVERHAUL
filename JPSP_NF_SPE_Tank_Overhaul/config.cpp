ccf5a1ff471f472dfeec53255338b4721e6c4392
jpspider1 <jp.spider1@gmail.com>
2026-02-05 15:39:36 -0500

    ~ Updated: armor, armorstructural, and hitpoints

--- JPSP_NF_SPE_Tank_Overhaul/config.cpp
+++ JPSP_NF_SPE_Tank_Overhaul/config.cpp
@@ -830,6 +830,8 @@ class CfgVehicles
 	//inherit end
 	class NORTH_FIN_T34_76_1941: NORTH_Tank_Base
 	{
+		armor = 450; //400
+		armorStructural = 50; //8
 		class Turrets: Turrets
 		{
 			class MainTurret: MainTurret
@@ -961,8 +963,9 @@ class CfgVehicles
 				material = -1;
 				name = "firegeo_hull";
 				visual = "damage_hull";
-				passThrough = 1;
+				passThrough = 10; //1
 				minimalHit = 0.14; //0.4
+				explosionShielding = 0.35; //SPE
 			};
 			class HitLTrack: HitLTrack
 			{
@@ -972,7 +975,8 @@ class CfgVehicles
 				name = "firegeo_track_L";
 				visual = "firegeo_track_L";
 				passThrough = 0;
-				minimalHit = 0.02;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
 			};
 			class HitRTrack: HitRTrack
 			{
@@ -982,7 +986,8 @@ class CfgVehicles
 				name = "firegeo_track_R";
 				visual = "firegeo_track_R";
 				passThrough = 0;
-				minimalHit = 0.02;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
 			};
 			class HitEngine: HitEngine
 			{
@@ -993,6 +998,7 @@ class CfgVehicles
 				visual = "firegeo_engine";
 				passThrough = 1; //0
 				minimalHit = 0.14; //0.3
+				explosionShielding = 0.4; //SPE
 
 				//SPE
                 class DestructEffects
@@ -1040,6 +1046,8 @@ class CfgVehicles
 	};
 	class NORTH_FIN_T34_76_1943: NORTH_Tank_Base
 	{
+		armor = 450; //420
+		armorStructural = 50; //8
 		class Turrets: Turrets
 		{
 			class MainTurret: MainTurret
@@ -1161,9 +1169,100 @@ class CfgVehicles
 				init = "(_this select 0) setVariable ['SPE_isTank',true];";
 			};
 		};
+		class HitPoints: HitPoints
+		{
+			class HitHull: HitHull
+			{
+				armor = -190; //1
+				radius = 0.16;
+				material = -1;
+				name = "firegeo_hull";
+				visual = "damage_hull";
+				passThrough = 10; //1
+				minimalHit = 0.14; //0.4
+				explosionShielding = 0.35; //SPE
+			};
+			class HitLTrack: HitLTrack
+			{
+				armor = 1;
+				radius = 0.18;
+				material = -1;
+				name = "firegeo_track_L";
+				visual = "firegeo_track_L";
+				passThrough = 0;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
+			};
+			class HitRTrack: HitRTrack
+			{
+				armor = 1;
+				radius = 0.18;
+				material = -1;
+				name = "firegeo_track_R";
+				visual = "firegeo_track_R";
+				passThrough = 0;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
+			};
+			class HitEngine: HitEngine
+			{
+				armor = 0.4; //1.5
+				radius = 0.14;
+				material = -1;
+				name = "firegeo_engine";
+				visual = "firegeo_engine";
+				passThrough = 1; //0
+				minimalHit = 0.14; //0.3
+				explosionShielding = 0.4; //SPE
+				
+				//SPE
+                class DestructEffects
+                {
+                    ammoExplosionEffect = "";
+					class SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "SmallWreckSmoke";
+                    };
+                    class SPE_Engine_Fire: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "SmallFireFPlace";
+                    };
+                    class SPE_Engine_Sounds: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "sound";
+                        type = "Fire";
+                    };
+                    class SPE_Engine_Sparks: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "FireSparks";
+                    };
+                };
+			};
+		};
 	};
 	class NORTH_FIN_T34_85_45: NORTH_Tank_Base
 	{
+		armor = 450; //420
+		armorStructural = 50; //8
 		class Turrets: Turrets
 		{
 			class MainTurret: MainTurret
@@ -1287,9 +1386,101 @@ class CfgVehicles
 				init = "(_this select 0) setVariable ['SPE_isTank',true];";
 			};
 		};
+		class HitPoints: HitPoints
+		{
+			class HitHull: HitHull
+			{
+				armor = -190; //1
+				radius = 0.16;
+				material = -1;
+				name = "firegeo_hull";
+				visual = "damage_hull";
+				passThrough = 10; //1
+				minimalHit = 0.14; //0.4
+				explosionShielding = 0.35; //SPE
+			};
+			class HitLTrack: HitLTrack
+			{
+				armor = 1;
+				radius = 0.18;
+				material = -1;
+				name = "firegeo_track_L";
+				visual = "firegeo_track_L";
+				passThrough = 0;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
+			};
+			class HitRTrack: HitRTrack
+			{
+				armor = 1;
+				radius = 0.18;
+				material = -1;
+				name = "firegeo_track_R";
+				visual = "firegeo_track_R";
+				passThrough = 0;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
+			};
+			class HitEngine: HitEngine
+			{
+				armor = 0.4; //1.5
+				radius = 0.14;
+				material = -1;
+				name = "firegeo_engine";
+				visual = "firegeo_engine";
+				passThrough = 1; //0
+				minimalHit = 0.14; //0.3
+				explosionShielding = 0.4; //SPE
+
+				
+				//SPE
+                class DestructEffects
+                {
+                    ammoExplosionEffect = "";
+					class SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "SmallWreckSmoke";
+                    };
+                    class SPE_Engine_Fire: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "SmallFireFPlace";
+                    };
+                    class SPE_Engine_Sounds: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "sound";
+                        type = "Fire";
+                    };
+                    class SPE_Engine_Sparks: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "FireSparks";
+                    };
+                };
+			};
+		};
 	};
 	class NORTH_FIN_T34_85: NORTH_Tank_Base
 	{
+		armor = 450; //420
+		armorStructural = 50; //8
 		class Turrets: Turrets
 		{
 			class MainTurret: MainTurret
@@ -1411,6 +1602,96 @@ class CfgVehicles
 				init = "(_this select 0) setVariable ['SPE_isTank',true];";
 			};
 		};
+		class HitPoints: HitPoints
+		{
+			class HitHull: HitHull
+			{
+				armor = -190; //1
+				radius = 0.16;
+				material = -1;
+				name = "firegeo_hull";
+				visual = "damage_hull";
+				passThrough = 10; //1
+				minimalHit = 0.14; //0.4
+				explosionShielding = 0.35; //SPE
+			};
+			class HitLTrack: HitLTrack
+			{
+				armor = 1;
+				radius = 0.18;
+				material = -1;
+				name = "firegeo_track_L";
+				visual = "firegeo_track_L";
+				passThrough = 0;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
+			};
+			class HitRTrack: HitRTrack
+			{
+				armor = 1;
+				radius = 0.18;
+				material = -1;
+				name = "firegeo_track_R";
+				visual = "firegeo_track_R";
+				passThrough = 0;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
+			};
+			class HitEngine: HitEngine
+			{
+				armor = 0.4; //1.5
+				radius = 0.14;
+				material = -1;
+				name = "firegeo_engine";
+				visual = "firegeo_engine";
+				passThrough = 1; //0
+				minimalHit = 0.14; //0.3
+				explosionShielding = 0.4; //SPE
+
+								
+				//SPE
+                class DestructEffects
+                {
+                    ammoExplosionEffect = "";
+					class SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "SmallWreckSmoke";
+                    };
+                    class SPE_Engine_Fire: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "SmallFireFPlace";
+                    };
+                    class SPE_Engine_Sounds: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "sound";
+                        type = "Fire";
+                    };
+                    class SPE_Engine_Sparks: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "FireSparks";
+                    };
+                };
+			};
+		};
 	};
 
 	/*
@@ -1468,7 +1749,9 @@ class CfgVehicles
 	};
 	*/
 	class NORTH_SOV_T70: NORTH_Tank_Base
-	{
+	{		
+		armor = 450; //420
+		armorStructural = 50; //8
 		class Turrets: Turrets
 		{
 			class MainTurret: MainTurret
@@ -1590,6 +1873,95 @@ class CfgVehicles
 			class SPE_VehicleTypeCaching
 			{
 				init = "(_this select 0) setVariable ['SPE_isTank',true];";
+			};
+		};
+		class HitPoints: HitPoints
+		{
+			class HitHull: HitHull
+			{
+				armor = -190; //1
+				radius = 0.16;
+				material = -1;
+				name = "firegeo_hull";
+				visual = "damage_hull";
+				passThrough = 10; //1
+				minimalHit = 0.14; //0.4
+				explosionShielding = 0.35; //SPE
+			};
+			class HitLTrack: HitLTrack
+			{
+				armor = 1;
+				radius = 0.18;
+				material = -1;
+				name = "firegeo_track_L";
+				visual = "firegeo_track_L";
+				passThrough = 0;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
+			};
+			class HitRTrack: HitRTrack
+			{
+				armor = 1;
+				radius = 0.18;
+				material = -1;
+				name = "firegeo_track_R";
+				visual = "firegeo_track_R";
+				passThrough = 0;
+				minimalHit = 0.07; //0.02
+				explosionShielding = 3; //SPE
+			};
+			class HitEngine: HitEngine
+			{
+				armor = 0.4; //1.5
+				radius = 0.14;
+				material = -1;
+				name = "firegeo_engine";
+				visual = "firegeo_engine";
+				passThrough = 1; //0
+				minimalHit = 0.14; //0.3
+				explosionShielding = 0.4; //SPE
+				
+				//SPE
+                class DestructEffects
+                {
+                    ammoExplosionEffect = "";
+					class SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "SmallWreckSmoke";
+                    };
+                    class SPE_Engine_Fire: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "SmallFireFPlace";
+                    };
+                    class SPE_Engine_Sounds: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "sound";
+                        type = "Fire";
+                    };
+                    class SPE_Engine_Sparks: SPE_Engine_Smoke
+                    {
+                        intensity = 0.5;
+                        interval = 1;
+                        lifeTime = 60;
+                        position = "engine_smoke";
+                        simulation = "particles";
+                        type = "FireSparks";
+                    };
+                };
 			};
 		};
 	};
