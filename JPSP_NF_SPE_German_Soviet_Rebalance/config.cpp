80e4c3232f0f9eb9908f0d5ba83a8f73bdf68ed0
jpspider1 <jp.spider1@gmail.com>
2026-02-05 17:28:39 -0500

    ~ Reduced: hit for German shells

--- JPSP_NF_SPE_German_Soviet_Rebalance/config.cpp
+++ JPSP_NF_SPE_German_Soviet_Rebalance/config.cpp
@@ -26,114 +26,114 @@ class CfgAmmo
 	// Panzer IV
 	class SPE_PzGr39_KWK40_AP: SPE_Shell_base
 	{
-		hit = 305; //205
+		hit = 205; //205
 		deflecting = 14; //29
 	};
 	class SPE_PzGr39_KWK40_AP_penetrator: SPE_ammo_APHE_penetrator
 	{
-		hit = 305; //205
+		hit = 205; //205
 		// All mm values multipled by 1.25 due to NF modeling
         MACRO_CALIBER(186.25,792) //149mm 792m/s
 	};
 
 	class SPE_PzGr40_KWK40_APCR: SPE_ShellAPCR_base
 	{
-		hit = 315; //215
+		hit = 215; //215
 		deflecting = 17; //34
 	};
 	class SPE_PzGr40_KWK40_APCR_penetrator: SPE_ammo_apcr_penetrator
 	{
-		hit = 315; //215
+		hit = 215; //215
 		MACRO_CALIBER(251.25,989) //201mm 989m/s
 	};
 	// Panther
 	class SPE_PzGr3942_KwK42_AP: SPE_Shell_base
 	{
-		hit = 330; //230
+		hit = 230; //230
 		deflecting = 14; //29
 	};
 	class SPE_PzGr3942_KwK42_AP_penetrator: SPE_ammo_APHE_penetrator
 	{
-		hit = 330; //230
+		hit = 230; //230
 		MACRO_CALIBER(236.25,935) //189mm 935/s
 	};
 
 	class SPE_PzGr4042_KwK42_APCR: SPE_ShellAPCR_base
 	{
-		hit = 340; //240
+		hit = 240; //240
 		deflecting = 17; //34
 	};
 	class SPE_PzGr4042_KwK42_APCR_penetrator: SPE_ammo_apcr_penetrator
 	{
-		hit = 340; //240
+		hit = 240; //240
 		MACRO_CALIBER(337.5,1120) //270mm 1120/s
 	};
 	// Tiger
 	class SPE_PzGr39_KwK36_AP: SPE_Shell_base
 	{
-		hit = 335; //235
+		hit = 235; //235
 		deflecting = 14; //29
 	};
 	class SPE_PzGr39_KwK36_AP_penetrator: SPE_ammo_APHE_penetrator
 	{
-		hit = 335; //235
+		hit = 235; //235
 		MACRO_CALIBER(207.5,779) //166mm 779/s
 	};
 	
 	class SPE_PzGr40_KwK36_APCR: SPE_ShellAPCR_base
 	{
-		hit = 350; //250
+		hit = 250; //250
 		deflecting = 17; //34
 	};
 	class SPE_PzGr40_KwK36_APCR_penetrator: SPE_ammo_apcr_penetrator
 	{
-		hit = 350; //250
+		hit = 250; //250
 		MACRO_CALIBER(275,930) //220mm 930/s
 	};
 
 	// Jagdpanther
 	class SPE_PzGr43_AP: SPE_Shell_base
 	{
-		hit = 340; //240
+		hit = 240; //240
 		deflecting = 14; //29
 	};
 	class SPE_PzGr43_AP_penetrator: SPE_ammo_APHE_penetrator
 	{
-		hit = 340; //240
+		hit = 240; //240
 		MACRO_CALIBER(293.75,1000) //235mm 1000/s
 	};
 
 	class SPE_PzGr40_APCR: SPE_ShellAPCR_base
 	{
-		hit = 360; //260
+		hit = 260; //260
 		deflecting = 17; //34
 	};
 	class SPE_PzGr40_APCR_penetrator: SPE_ammo_apcr_penetrator
 	{
-		hit = 360; //260
+		hit = 260; //260
 		MACRO_CALIBER(386.25,1130) //309mm 1130/s
 	};
 
 	// Panzer III J, L, M
 	class SPE_50mm_PzGr39_AP: SPE_Shell_base
 	{
-		hit = 295; //195
+		hit = 195; //195
 		deflecting = 14; //29
 	};
 	class SPE_50mm_PzGr39_AP_penetrator: SPE_ammo_APHE_penetrator
 	{
-		hit = 295; //195
+		hit = 195; //195
 		MACRO_CALIBER(131.25,834) //105mm 834/s
 	};
 
 	class SPE_50mm_PzGr40_APCR: SPE_ShellAPCR_base
 	{
-		hit = 295; //195
+		hit = 195; //195
 		deflecting = 17; //34
 	};
 	class SPE_50mm_PzGr40_APCR_penetrator: SPE_ammo_apcr_penetrator
 	{
-		hit = 295; //195
+		hit = 195; //195
 		MACRO_CALIBER(176.25,1198) //141mm 1198/s
 	};
 
