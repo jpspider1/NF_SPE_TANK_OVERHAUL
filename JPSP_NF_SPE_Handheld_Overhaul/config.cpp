eeec408df29722d476a1fbd3727161f0be108513
jpspider1 <jp.spider1@gmail.com>
2026-07-06 09:26:32 -0400

    Feat(Fausts and schrecks): Increased hit to 350

--- JPSP_NF_SPE_Handheld_Overhaul/config.cpp
+++ JPSP_NF_SPE_Handheld_Overhaul/config.cpp
@@ -23,18 +23,18 @@ class CfgAmmo
 	class SPE_PzFaust_30m_penetrator: SPE_HEAT_Rocket_penetrator_Base
 	{
         MACRO_CALIBER(250,1000) //originally 200mm and 1000m/s in SPE. mm value multipled by 1.25
-		hit = 250;
+		hit = 350; //originally 250 upped to 350
 	};
 
 	class SPE_PzFaust_60m_penetrator: SPE_HEAT_Rocket_penetrator_Base
 	{
         MACRO_CALIBER(250,1000) //originally 200mm and 1000m/s in SPE. mm value multipled by 1.25
-		hit = 250;
+		hit = 350; //originally 250 upped to 350
 	};
 
 	class SPE_R_88mm_RPzBGr_penetrator: SPE_HEAT_Rocket_penetrator_Base
 	{
 		MACRO_CALIBER(237.5,1000)  //originall 190mm and 1000m/s in SPEmm value multipled by 1.25
-		hit = 250;
+		hit = 350; //originally 250 upped to 350
 	};
 };
